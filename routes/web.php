615f8696f2080a242385919b2638f54aee79d782
Luis <cristian.98oruro@gmail.com>
2025-01-10 14:46:07 -0400

    ya con el primer commit de fusion

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\AngularController;
 use Illuminate\Support\Facades\Route;
-
+Route::any('/{any}', [AngularController::class, 'index'])->where('any', '^(?!api).*$');
 // Route::resource('api/Clientes', 'App\Http\Controllers\ClienteController');
 // Route::get('/', function () {
 //     return view('welcome');
